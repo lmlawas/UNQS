8ca45ba114d53aba50a8c040c8228165a5fe4feb
lmlawas <lmlawas@up.edu.ph>
2017-05-14 00:18:17 +0800

    Add main program

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+	To compile:
+		javac -cp ".;mysql-connector-java.jar;" *.java
+	To run:
+		java -cp .:mysql-connector-java.jar Main test.conf
+*/
+import java.io.Console;
+import java.io.FileWriter;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main{
+
+	public static void main(String args[]){
+		
+	}
+}
