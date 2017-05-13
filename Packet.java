a50d40a3b1c1b30b5a5ae113dcfa69cff6d9a7ec
lmlawas <lmlawas@up.edu.ph>
2017-05-14 00:16:46 +0800

    Remove weight attribute

--- Packet.java
+++ Packet.java
@@ -6,19 +6,17 @@ public class Packet{
 	public int priority;
 	public double size;
 	public int timeout;
-	public int wait_time;	
-	public int weight;
+	public int wait_time;
 
 	// constructors
 	public Packet(){		
 	}
 
-	public Packet(int id, int priority, double size, int timeout, int wait_time, int weight){
+	public Packet(int id, int priority, double size, int timeout, int wait_time){
 		this.id = id;
 		this.priority = priority;
 		this.size = size;
 		this.timeout = timeout;
 		this.wait_time = wait_time;
-		this.weight = weight;
 	}
 }
