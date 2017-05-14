d98f7bb22a0755afa748fb64d44390ab09fed63e
lmlawas <lmlawas@up.edu.ph>
2017-05-14 09:42:40 +0800

    Add configuration logic

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 	To compile:
 		javac -cp ".;mysql-connector-java.jar;" *.java
 	To run:
-		java -cp .:mysql-connector-java.jar Main test.conf
+		java -cp .:mysql-connector-java.jar Main simulation.conf
 */
 import java.io.Console;
 import java.io.FileWriter;
@@ -13,6 +13,59 @@ import java.util.Scanner;
 public class Main{
 
 	public static void main(String args[]){
+		Console sensitive = System.console();
+		Scanner input = new Scanner( System.in );
+		String choice, s;
+		int read = 0;
 		
+		Configuration config = new Configuration();
+		config.setDefault();
+
+		if(args.length == 0){
+			do{
+				System.out.print("Continue with the default configuration? (Y/N) ");
+				choice = input.nextLine();
+
+				if( choice.equalsIgnoreCase("y") || choice.equalsIgnoreCase("yes") ){
+					read = 1;
+					break;
+				}
+
+				else if(choice.equalsIgnoreCase("n") || choice.equalsIgnoreCase("no")){
+					System.out.println("Please re-run program with a configuration file. (ex. Main test.conf)");
+					return;
+				}
+
+				else{
+					System.out.println("Invalid choice. Try again.");
+				}
+			}while(true);			
+		}
+
+		else if(args.length > 1){
+			System.out.println("Error. Too many arguments. Please re-run program.");
+			return;
+		}
+
+		/* configure using .conf file */
+		else if( args.length == 1 ){
+			
+			read = config.readFile( args[0] );
+			
+			if(read == 1){
+				
+				if( config.hasPassword() ){
+					if( sensitive == null ) return;
+					System.out.print("MySQL password: ");
+				    s = String.valueOf( sensitive.readPassword() );
+				    config.setPassword( s );
+				}
+			}
+		}
+
+		if(read == 1){
+			config.show();
+		}
+
 	}
 }
